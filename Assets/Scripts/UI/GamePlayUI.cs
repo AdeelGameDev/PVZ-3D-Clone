70675c85046554bc64600ef53e9029eba67e91aa
Adeel <adeelthedeveloper@gmail.com>
2025-03-31 00:11:16 +0500

    PausedUI

--- Assets/Scripts/UI/GamePlayUI.cs
+++ Assets/Scripts/UI/GamePlayUI.cs
@@ -4,19 +4,22 @@ using UnityEngine.UI;
 public class GamePlayUI : MonoBehaviour
 {
     [SerializeField] private Button pauseButton;
+    [SerializeField] private RectTransform pausedUI;
     private bool isPaused = false;
     private AudioSource[] allAudioSources;
 
     void Start()
     {
         pauseButton.onClick.AddListener(TogglePause);
-        allAudioSources = FindObjectsOfType<AudioSource>(); // Get all audio sources
+        allAudioSources = FindObjectsOfType<AudioSource>();
+        pausedUI.gameObject.SetActive(false); // Ensure paused UI starts hidden
     }
 
     private void TogglePause()
     {
         isPaused = !isPaused;
         Time.timeScale = isPaused ? 0 : 1;
+        pausedUI.gameObject.SetActive(isPaused); // Show/hide paused UI
 
         foreach (var audio in allAudioSources)
         {
